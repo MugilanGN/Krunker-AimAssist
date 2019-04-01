498c5bdf25676dedb821c9b73cb9e7cc57990039
Mugilan Ganesan <49064156+MugilanGN@users.noreply.github.com>
2019-04-01 16:57:16 +0530

    Add files via upload

---
+++ New AA.py
@@ -0,0 +1,96 @@
+import cv2 
+import numpy as np
+import mss
+import pyautogui
+import time
+
+
+def scrnshot(monitor):
+    
+    with mss.mss() as sct:
+        frame = np.array(sct.grab(monitor))
+        
+    return frame
+
+
+def find_bar(frame):
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower = np.array([0,80,100])
+    upper = np.array([0,255,255])
+         
+    mask = cv2.inRange(hsv, lower, upper)
+        
+    (cnts, _) = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:1]
+    
+    return cnts
+
+
+def process(cnts,frame):
+    
+    for c in cnts:
+        
+        x,y,w,h = cv2.boundingRect(c)
+        
+        if h<9:
+            w = int(h*8.5)
+            cen = int((x + x+h)/2)
+
+            draw_rect(x,y,w,h,cen,frame)
+            mouse(x,y,cen,h)
+            
+        else:
+            pass
+
+
+def draw_rect(x,y,w,h,cen,frame):
+
+    cen2 = int(cen+(w/2))
+    
+    cv2.rectangle(frame, (x,y), (x+w,y+h), (255,255,0), 2)
+    cv2.rectangle(frame, ( cen2+20 , y+(h*2) ), ( cen+5 , y+40 ), (0,255,0), 2)
+
+            
+def mouse(x,y,cen,h):
+    global width, height
+    mx = (pyautogui.position().x) + cen - (width/2)
+    my = (pyautogui.position().y) + y+(h*2) - (height/2) + 15
+
+    pyautogui.moveTo(mx, my)
+
+
+def cleanup(frame):
+    cv2.imshow('the damn window',frame)
+        
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('q'):
+        cv2.destroyAllWindows()
+        quit()
+
+    
+def main():
+    global width, height
+    
+    width = 450
+    height = 450
+    
+    monitor = {"top" : 400, "left" : 950, "width" : width, "height" : height}
+
+    frame = scrnshot(monitor)
+
+    cnts = find_bar(frame)
+
+    if len(cnts) > 0:
+        process(cnts,frame)
+        
+    else:
+        pass
+    
+    cleanup(frame)
+
+
+while True:
+    main()
+    
